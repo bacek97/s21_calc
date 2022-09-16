4d5568fe7544a52edf0b9cbf6018ff32d0733a94
Gladis Ariane <gladisar@student.21-school.ru>
2022-09-16 10:29:24 +0300

    Кнопки со всеми операциями. Сохраняется ввод в поле.

--- main.c
+++ main.c
@@ -4,134 +4,171 @@
 //#include "GL/glxew.h"
 //#include <stdio.h>
 
-enum radioOptions {
-  EASY,
-  HARD
-};
-
 struct my_nkc_app {
   struct nkc *nkcHandle;
-
-  /* some user data */
-  float value;
-  enum radioOptions op;
-  struct nk_colorf cb_color;
+  /*data */
+  char input[256];
+  size_t input_size;
+  size_t current_input_index;
 };
 void mainLoop(void *loopArg) {
 
-  struct my_nkc_app *myapp = (struct my_nkc_app *) loopArg;
-  struct nk_context *ctx = nkc_get_ctx(myapp->nkcHandle);
+  struct my_nkc_app *global_context = (struct my_nkc_app *) loopArg;
+  struct nk_context *ctx = nkc_get_ctx(global_context->nkcHandle);
 
-  union nkc_event e = nkc_poll_events(myapp->nkcHandle);
+  union nkc_event e = nkc_poll_events(global_context->nkcHandle);
   if ((e.type == NKC_EWINDOW) && (e.window.param == NKC_EQUIT)) {
-    nkc_stop_main_loop(myapp->nkcHandle);
+    nkc_stop_main_loop(global_context->nkcHandle);
   }
 
   int x;
   int y;
-  glfwGetWindowSize(myapp->nkcHandle->window, &x, &y);
+  glfwGetWindowSize(global_context->nkcHandle->window, &x, &y);
 
 #ifdef NK_GLFW_GL3_MOUSE_GRABBING
-  struct nk_vec2 mpos = myapp->nkcHandle->ctx->input.mouse.pos;
+  struct nk_vec2 mpos = global_context->nkcHandle->ctx->input.mouse.pos;
   if (mpos.x && mpos.y){
     printf("%f\n",mpos.x);
   }
 #endif
   /* Nuklear GUI code */
-
+  char buffer[global_context->input_size];
+  memcpy(buffer, global_context->input, global_context->input_size);
   if (nk_begin(ctx, "Calculator", nk_rect(0, 0, x, y),
-               NK_WINDOW_BORDER
+               /*NK_WINDOW_BORDER |*/ NK_WINDOW_DYNAMIC
   )) {
-    static int set = 0, prev = 0, op = 0;
-    static const char numbers[] = "789456123";
-    static const char ops[] = "+-*/";
-    static double a = 0, b = 0;
-    static double *current = &a;
-
-    size_t i = 0;
-    float w = nk_window_get_width(ctx);
     float h = nk_window_get_height(ctx);
+    float row_h = h / 7;
+    nk_layout_row_dynamic(ctx, row_h-15, 1);
 
-    int solve = 0;
-    {
-      int len;
-      char buffer[256];
-      nk_layout_row_dynamic(ctx, h / 5 - 20, 1);
-      len = snprintf(buffer, 256, "%.2f", *current);
-      nk_edit_string(ctx, NK_EDIT_SIMPLE, buffer, &len, 255, nk_filter_float);
-      buffer[len] = 0;
-      *current = atof(buffer);
+    nk_edit_string(ctx,
+                   NK_EDIT_READ_ONLY,
+                   buffer,
+                   &global_context->current_input_index,
+                   global_context->input_size,
+                   nk_filter_ascii);
+
+    nk_layout_row_dynamic(ctx, row_h, 5);
+    int i = 1;
+    char c[2] = "";
+    for (i; i < 4; ++i) {
+      nk_itoa(c, i);
+      if (nk_button_label(ctx, c)) {
+        strcat(buffer, c);
+        global_context->current_input_index++;
+      }
+
+    }
+    if (nk_button_label(ctx, "(")) {
+      strcat(buffer, "(");
+      global_context->current_input_index++;
+    }
+    if (nk_button_label(ctx, ")")) {
+      strcat(buffer, ")");
+      global_context->current_input_index++;
+    }
+    for (; i < 7; ++i) {
+      nk_itoa(c, i);
+      if (nk_button_label(ctx, c)) {
+        strcat(buffer, c);
+        global_context->current_input_index++;
+      }
+    }
+    if (nk_button_label(ctx, "+")) {
+      strcat(buffer, "+");
+      global_context->current_input_index++;
+    }
+    if (nk_button_label(ctx, "-")) {
+      strcat(buffer, "-");
+      global_context->current_input_index++;
+    }
+    for (; i < 10; ++i) {
+      nk_itoa(c, i);
+      if (nk_button_label(ctx, c)) {
+        strcat(buffer, c);
+        global_context->current_input_index++;
+      }
+    }
+    if (nk_button_label(ctx, "*")) {
+      strcat(buffer, "*");
+      global_context->current_input_index++;
+    }
+    if (nk_button_label(ctx, "/")) {
+      strcat(buffer, "/");
+      global_context->current_input_index++;
+    }
+    if (nk_button_label(ctx, "0")) {
+      strcat(buffer, "0");
+      global_context->current_input_index++;
+    }
+    if (nk_button_label(ctx, ".")) {
+      strcat(buffer, ".");
+      global_context->current_input_index++;
     }
 
-    nk_layout_row_dynamic(ctx, h / 5 - 20, 4);
-
-    for (i = 0; i < 16; ++i) {
-      if (i >= 12 && i < 15) {
-        if (i > 12) continue;
-        if (nk_button_label(ctx, "C")) {
-          a = b = op = 0;
-          current = &a;
-          set = 0;
-        }
-        if (nk_button_label(ctx, "0")) {
-          *current = *current * 10.0f;
-          set = 0;
-        }
-        if (nk_button_label(ctx, "=")) {
-          solve = 1;
-          prev = op;
-          op = 0;
-        }
-      } else if (((i + 1) % 4)) {
-        if (nk_button_text(ctx, &numbers[(i / 4) * 3 + i % 4], 1)) {
-          *current = *current * 10.0f + numbers[(i / 4) * 3 + i % 4] - '0';
-          set = 0;
-        }
-      } else if (nk_button_text(ctx, &ops[i / 4], 1)) {
-        if (!set) {
-          if (current != &b) {
-            current = &b;
-          } else {
-            prev = op;
-            solve = 1;
-          }
-        }
-        op = ops[i / 4];
-        set = 1;
-      }
+    if (nk_button_label(ctx, "pow")) {
+      strcat(buffer, "pow");
+      global_context->current_input_index += 3;
+    }
+    if (nk_button_label(ctx, "mod")) {
+      strcat(buffer, "mod");
+      global_context->current_input_index += 3;
+    }
+    if (nk_button_label(ctx, "cos")) {
+      strcat(buffer, "cos");
+      global_context->current_input_index += 3;
+    }
+    if (nk_button_label(ctx, "sin")) {
+      strcat(buffer, "sin");
+      global_context->current_input_index += 3;
+    }
+    if (nk_button_label(ctx, "tan")) {
+      strcat(buffer, "tan");
+      global_context->current_input_index += 3;
+    }
+    if (nk_button_label(ctx, "acos")) {
+      strcat(buffer, "acos");
+      global_context->current_input_index += 4;
+    }
+    if (nk_button_label(ctx, "asin")) {
+      strcat(buffer, "asin");
+      global_context->current_input_index += 4;
+    }
+    if (nk_button_label(ctx, "atan")) {
+      strcat(buffer, "atan");
+      global_context->current_input_index += 4;
+    }
+    if (nk_button_label(ctx, "sqrt")) {
+      strcat(buffer, "sqrt");
+      global_context->current_input_index += 4;
+    }
+    if (nk_button_label(ctx, "ln")) {
+      strcat(buffer, "ln");
+      global_context->current_input_index += 2;
+    }
+    if (nk_button_label(ctx, "log")) {
+      strcat(buffer, "log");
+      global_context->current_input_index += 3;
     }
 
     if (e.type == NKC_EKEY) {
-
-      const int scancode = glfwGetKeyScancode(e.key.code);
-      printf("%d\n", scancode);
-      printf("%d\n", e.key.code);
-      //printf("%d\n", e.key.mod);
-      //const char *name = glfwGetKeyName(e.key.code, 0);
-      //puts(name);
       switch (e.key.code) {
-        default:break;
-        case NKC_KEY_0:printf("%c\n", nkc_get_key_char(e.key.code));
+        default:printf("%d\n",e.key.code);
           break;
-        case NKC_KEY_1:printf("%c\n", nkc_get_key_char(e.key.code));
+        case NKC_KEY_0:
+        case NKC_KEY_1:
+        case NKC_KEY_2:
+        case NKC_KEY_3:
+        case NKC_KEY_4:
+        case NKC_KEY_5:
+        case NKC_KEY_6:
+        case NKC_KEY_7:
+        case NKC_KEY_8:
+        case NKC_KEY_9:
+          buffer[global_context->current_input_index++] = e.key.code;
           break;
-        case NKC_KEY_2:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_3:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_4:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_5:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_6:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_7:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_8:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_9:printf("%c\n", nkc_get_key_char(e.key.code));
-          break;
-        case NKC_KEY_DELETE:printf("%c\n", 'c');
+        case NKC_KEY_DELETE:buffer[0] = 0;
+          global_context->current_input_index = 0;
           break;
         case NKC_KEY_E:printf("%c\n", '=');
           break;
@@ -139,52 +176,46 @@ void mainLoop(void *loopArg) {
           break;
         case NKC_KEY_M:printf("%c\n", '*');
           break;
-        case NKC_KEY_A:printf("%c\n",     '+');
+        case NKC_KEY_A:printf("%c\n", '+');
           break;
-        case NKC_KEY_D:printf("%c\n",  '/');
+        case NKC_KEY_D:printf("%c\n", '/');
           break;
-        case NKC_KEY_BACKSPACE:printf("%c\n",  'd');
+        case NKC_KEY_BACKSPACE:
+          buffer[global_context->current_input_index--] = 0;
+
           break;
       }
     }
-
-    if (solve) {
-      if (prev == '+') a = a + b;
-      if (prev == '-') a = a - b;
-      if (prev == '*') a = a * b;
-      if (prev == '/') a = a / b;
-      current = &a;
-      if (set) current = &b;
-      b = 0;
-      set = 0;
-    }
+    memcpy(global_context->input, buffer, global_context->input_size);
   }
   nk_end(ctx);
 /* End Nuklear GUI */
 
-  nkc_render(myapp->nkcHandle, nk_rgb(0, 0, 0));
+  nkc_render(global_context->nkcHandle, nk_rgb(0, 0, 0));
 }
 
-
-
 int main() {
-  struct my_nkc_app myapp;
+  struct my_nkc_app global_context;
   struct nkc nkcx; /* Allocate memory for Nuklear+ handle */
-  myapp.nkcHandle = &nkcx;
-  /* init some user data */
-  myapp.value = 0.4;
-  myapp.op = HARD;
-
-  if (nkc_init(myapp.nkcHandle,
+  global_context.nkcHandle = &nkcx;
+  /* user data */
+  global_context.input_size = 256;
+  global_context.current_input_index = 0;
+  global_context.input[0] = '\0';
+  //memset(global_context.input, ' ', global_context.input_size);
+  global_context.input[global_context.input_size - 1] = 0;
+  /*user data end*/
+  if (nkc_init(global_context.nkcHandle,
                "Calculator",
                1280,
                720,
                NKC_WIN_MAXIMIZED)) {
-    printf("Successfull init. Starting 'infinite' main loop...\n");
-    nkc_set_main_loop(myapp.nkcHandle, mainLoop, (void *) &myapp);
+    nkc_set_main_loop(global_context.nkcHandle,
+                      mainLoop,
+                      (void *) &global_context);
   } else {
     printf("Can't init NKC\n");
   }
-  nkc_shutdown(myapp.nkcHandle);
+  nkc_shutdown(global_context.nkcHandle);
   return 0;
 }
